74bcb9e8da6c10d44a6c9e0a16ee2ca37bd4ec66
okabasele <ocekabasele@gmail.com>
2024-03-28 12:47:16 +0100

    Add dependencies, update verifyUser middleware, and send confirmation email

--- src/middlewares/sendMail.js
+++ src/middlewares/sendMail.js
@@ -1,30 +1,37 @@
 const brevo = require('@getbrevo/brevo');
+require('dotenv').config();
 
-const sendMail = (params) => {
-  const { receiver_email, receiver_name, subject, content, sender_name,  } = params;
-  let defaultClient = brevo.ApiClient.instance;
-
-  let apiKey = defaultClient.authentications['api-key'];
-  apiKey.apiKey = `xkeysib-${process.env.API_KEY}`;
+const sendMail = (receiver_email, receiver_name, subject, content, sender_name) => {
 
   let apiInstance = new brevo.TransactionalEmailsApi();
+
+  let apiKey = apiInstance.authentications['apiKey'];
+  apiKey.apiKey = process.env.API_KEY;
+  
+console.log({apikey: apiKey.apiKey})
   let sendSmtpEmail = new brevo.SendSmtpEmail();
 
   sendSmtpEmail.subject = `${subject}`;
-  sendSmtpEmail.htmlContent = `<html><body><h1></h1>${content}</body></html>`;
+  sendSmtpEmail.htmlContent = `<html><body><h1>${subject}</h1>${content}</body></html>`;
   sendSmtpEmail.sender = { "name": `${sender_name}`, "email": "ami95190@gmail.com" };
   sendSmtpEmail.to = [
     { "email": `${receiver_email}`, "name": `${receiver_name}` }
   ];
-  sendSmtpEmail.replyTo = { "email": "example@brevo.com", "name": "sample-name" };
+  sendSmtpEmail.replyTo =  { "email": `${receiver_email}`, "name": `${receiver_name}` };
   sendSmtpEmail.headers = { "Some-Custom-Name": "unique-id-1234" };
   sendSmtpEmail.params = { "parameter": "My param value", "subject": "common subject" };
 
 
   apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data) {
-    console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+    return {
+      success: true,
+      data
+    }
   }, function (error) {
-    console.error(error);
+      return {
+        success: false,
+        data: error
+      }
   });
 
 }
