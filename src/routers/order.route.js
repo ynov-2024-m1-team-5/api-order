9d7ebceef6fc6c9f4b8dc02f2cf8cb01a1d6d0fd
okabasele <ocekabasele@gmail.com>
2024-04-12 11:21:13 +0200

    ♻️ Refactor order creation and user verification

--- src/routers/order.route.js
+++ src/routers/order.route.js
@@ -1,13 +1,10 @@
 const orderController = require('../controllers/order.controller');
 const express = require('express');
 const router = express.Router();
-const verifyUser = require('../middlewares/verifyUser');
 const verifyIsAdmin = require('../middlewares/verifyIsAdmin');
 
 
 router.get('/',verifyIsAdmin, orderController.getAllOrders);
 router.get('/:order_id',verifyIsAdmin, orderController.getOrderById);
-router.post('/:customer_id',verifyUser, orderController.createOrder);
-// router.patch('/:customer_id/:order_id',verifyUser, orderController.confirmOrder);
 
 module.exports = router;
