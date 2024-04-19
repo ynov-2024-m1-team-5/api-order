d5fe0fede73dea965440ebe3a6f32fbb61bdaa04
okabasele <ocekabasele@gmail.com>
2024-04-19 16:49:48 +0200

    🚑 Update customer.route.js to use verifyIsAllowed middleware for getOrdersByCustomerId route

--- src/routers/customer.route.js
+++ src/routers/customer.route.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 const verifyUser = require('../middlewares/verifyUser');
 const verifyIsAdmin = require('../middlewares/verifyIsAdmin');
+const verifyIsAllowed = require('../middlewares/verifyIsAllowed');
 
-router.get('/:customer_id/orders', verifyUser, orderController.getOrdersByCustomerId);
+router.get('/:customer_id/orders', verifyIsAllowed, orderController.getOrdersByCustomerId);
 router.post('/:customer_id/orders/:order_id/refunded',verifyIsAdmin, orderController.refunded);
 router.post('/:customer_id/orders/:order_id/refund', verifyUser, orderController.refund);
 router.post('/:customer_id',verifyUser, orderController.createOrder);
