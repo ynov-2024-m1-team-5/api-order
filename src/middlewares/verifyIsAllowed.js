58750901dc166804248b50098e2ba9b7d75d34ca
okabasele <ocekabasele@gmail.com>
2024-04-19 17:27:20 +0200

    Refactor verifyIsAllowed middleware to include customer_id in the request parameters

--- src/middlewares/verifyIsAllowed.js
+++ src/middlewares/verifyIsAllowed.js
@@ -1,4 +1,5 @@
 const verifyIsAllowed = (req, res, next) => {
+  const { customer_id } = req.params;
   if ((!req.userToken.is_admin) && (customer_id != req.userToken.customer_id)) {
     console.log({token: req.userToken})
       return res.status(403).json({ success:false, message: "You must be an admin" });
