4a1be2e283d6535e2e3de93df5b368d660235648
okabasele <ocekabasele@gmail.com>
2024-04-19 17:02:01 +0200

    Refactor verifyIsAdmin middleware to verifyIsAllowed in verifyIsAllowed.js

--- src/middlewares/verifyIsAllowed.js
+++ src/middlewares/verifyIsAllowed.js
@@ -1,8 +1,9 @@
-const verifyIsAdmin = (req, res, next) => {
+const verifyIsAllowed = (req, res, next) => {
   if ((customer_id != req.userToken.customer_id) || (!req.userToken.is_admin) ) {
+    console.log({token: req.userToken})
       return res.status(403).json({ success:false, message: "You must be an admin" });
   }
   next();
 }
 
-module.exports = verifyIsAdmin;
+module.exports = verifyIsAllowed;
